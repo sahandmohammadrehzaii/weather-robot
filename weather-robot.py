9f8ca665eb29153ee84aebba012cf3d44dc047e6
sahandmohammadrezaii <139042771+sahandmohammadrehzaii@users.noreply.github.com>
2023-07-28 00:08:11 +0330

    Add files via upload

---
+++ weather-robot.py
@@ -0,0 +1,37 @@
+# Information Robot
+for i in range(1):
+   print("Hello")
+for i in range(1):
+   print("I am Robot")
+for i in range(1):
+   print("My name is Esteratous")
+for i in range(1):
+   print("I am a Robot Weather")
+for i in range(1):
+   print("My job is to take the temperature from you and tell you the type of weather and my advice to you on what clothes you should wear and what you should do in each situation.")
+# Information Ask Weather
+x=input("You Can Enter Number =")
+x=float(x)
+# Information Temperature value
+# Information Weather Cold
+if x<30:
+   print("Weather Is Cold")
+   print("I recommend that you wear warm clothes and have tea and warm food ")
+   print("If you catch a cold, you should rest, if not, be careful not to get sick in the cold")
+# Information Weather Hot
+if x>30:
+   print("Weather Is Hot")
+   print("I recommend that you wear thin clothes and drink more fluids")
+   print("If you go out in the heat, you may get heatstroke and you may feel bad, so when you go out with yourself take water")
+# Information Weather Mild
+if x<30 and x>30:
+   print("Weather Is Mild")
+   print("I dont have advice")
+   print("Take care of your health")
+# Information finish work
+for i in range(1):
+   print("Goodbye")
+# ("Hello Sahand Mohamad Rezaiee") 
+
+for i in range(1):
+   print("Hello sahand mohammad rezaiee")
